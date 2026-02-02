5a8e32071bb24437c9af0d246e5f04c2b8ffd5d7
José Manuel Arellanes <dev@josearellanes.me>
2026-02-02 00:54:29 -0600

    refactor: Use img tag for project previews
    Switches from using background images to a dedicated `<img>` element for project previews.
    
    This improves semantic HTML and offers more direct control over image attributes. It also includes pre-loading of all project images to enhance user experience by reducing load times on hover.
    
    Updates associated JavaScript to manipulate the `<img>` src and CSS to style the new image element.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,6 +5,14 @@ gsap.set(".project-preview", { width: 0 });
 const progressBar = document.querySelector(".progressbar");
 const projectPreview = document.querySelector(".project-preview");
 const navItems = document.querySelectorAll(".navigation-item");
+const previewImg = document.querySelector("#preview-img");
+const totalImages = 8;
+
+for (let i = 1; i <= totalImages; i++) {
+  const imgUrl = new URL(`../assets/img-${i}.webp`, import.meta.url).href;
+  const img = new Image();
+  img.src = imgUrl;
+}
 
 navItems.forEach((item) => {
   item.addEventListener("mouseenter", () => {
@@ -28,15 +36,16 @@ navItems.forEach((item) => {
   });
 });
 
-document.querySelectorAll(".navigation-link").forEach((link) => {
-  link.addEventListener("mouseenter", () => {
+document.querySelectorAll(".navigation-item").forEach((item) => {
+  item.addEventListener("mouseenter", () => {
+    const link = item.querySelector("a");
     const match = link.className.match(/navigation-link-(\d+)/);
 
     if (match) {
       const imgNumber = match[1];
       const imgUrl = new URL(`../assets/img-${imgNumber}.webp`, import.meta.url).href;
 
-      projectPreview.style.backgroundImage = `url(${imgUrl})`;
+      previewImg.src = imgUrl;
     }
   });
 });
